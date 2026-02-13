eedcc0dbe77e3b3719235863afd0727e9ebcfd18
AiFahri <fahrinurilll@gmail.com>
2026-02-13 14:18:11 +0700

    feat: add custom font faces and update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.tsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["BDO Grotesk", ...defaultTheme.fontFamily.sans],
+                monument: ['"Monument Extended"', "sans-serif"],
+                clash: ['"Clash Display"', "sans-serif"],
+                bdo: ['"BDO Grotesk"', "sans-serif"],
+            },
+            colors: {
+                navy: {
+                    800: "#0E2444",
+                    900: "#0B1E3B",
+                    950: "#071530",
+                },
+                "accent-red": "#D50000",
             },
         },
     },
